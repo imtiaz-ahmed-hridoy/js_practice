8da1719d83f22de45b33783e003e2d96f3997cac
Imtiaz Ahmed Hridoy <imtiazahmed6265@gmail.com>
2025-02-10 17:44:39 +0600

    Create js_practice.js

---
+++ js_practice.js
@@ -0,0 +1,47 @@
+
+// for(var i=1;i<=5;i++){
+//     var pattern ='';
+//   for(var j=1;j<=i;j++){
+//     pattern = pattern +' *'
+    
+//   }
+//   console.log(pattern);
+// }
+
+
+
+
+// let calculator =(num1,num2,operator)=>{
+  
+//   switch (operator) {
+//     case '+':
+//       return num1+num2;
+//     case '-':
+//       return num1-num2;
+//     case '*':
+//       return num1*num2;
+//     case '/':
+//       if(num2===0){
+//         return '0 is not allow'
+//       }else{
+//       return num1/num2;}
+//     default:
+//     return'there is no operator';
+//   }  
+// }
+
+
+
+// console.log(calculator(10,2,'/'));
+
+
+// const ispalindrome = (str)=>{
+//   let reverse='';
+//   for(char=str.length-1;char>=0;char--){
+//     reverse= reverse+str[char];
+//   }
+//   return str===reverse?true:false;
+// }
+
+
+// console.log(ispalindrome('level'));
